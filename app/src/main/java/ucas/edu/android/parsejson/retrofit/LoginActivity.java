ad3ca67d47f28e0973fc5d5a25ce33abdae1aaaf
Dev Abir <abirabullah@gmail.com>
2022-12-01 10:19:24 +0200

    retrofit

--- app/src/main/java/ucas/edu/android/parsejson/retrofit/LoginActivity.java
+++ app/src/main/java/ucas/edu/android/parsejson/retrofit/LoginActivity.java
@@ -5,26 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import ucas.edu.android.parsejson.R;
+import ucas.edu.android.parsejson.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     ApiService retrofitInstance;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         sharedPreferences = getSharedPreferences("sp",MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
         retrofitInstance = ApiService.RetrofitClass.getRetrofitInstance();
-        login();
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
 
     private void logOut(){
